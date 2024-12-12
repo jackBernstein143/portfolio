70c7b57b6f46d96a32ef6b1d745765d3f39e8652
Jack Bernstein <jackwolfebernstein@gmail.com>
2024-12-11 18:11:39 -0800

    Added camera permissions to Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    headers: {
+      'Permissions-Policy': 'camera=*',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      'Cross-Origin-Opener-Policy': 'same-origin'
+    }
+  }
 })
